dbd523629209d28449d4158d05f36c8af1e2bf91
michaelwahl <michaelwahl9@gmail.com>
2020-02-06 22:07:26 -0600

    create UI class with method to append shapes to DOM

--- app.js
+++ app.js
@@ -28,15 +28,6 @@ class Square{
     }
 }
 
-// const circle1 = new Circle(3);
-// const circle2 = new Circle(2);
-// const circle3 = new Circle(4);
-// const square1 = new Square(3);
-// const square2 = new Square(2);
-// const square3 = new Square(4);
-
-// const shapeArray = [circle1, circle2, circle3, square1, square2, square3]
-
 // Shape Generator Class
 class Generator{
     static sortShapes(shapeArray){
@@ -57,5 +48,18 @@ class Generator{
 
     static randomizeBetween(min, max){
         return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
+
+// UI Class
+class UI{
+    static displayShapes(shapeArray){
+        const shapeContainer = document.getElementById('shape-container');
+        let item = '';
+        shapeArray.forEach(shapeObj => {
+            item = document.createElement('div'); 
+            item.classList.add('shape', shapeObj.constructor.name.toLowerCase())
+            shapeContainer.appendChild(item);
+        })
     }
 }
