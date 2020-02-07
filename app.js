f3f1aea46f3b93658371c6fdc598d2fec559130e
michaelwahl <michaelwahl9@gmail.com>
2020-02-06 20:41:43 -0600

    create generator class

--- app.js
+++ app.js
@@ -27,3 +27,35 @@ class Square{
         return `Square: Length = ${this.length}, Area = ${this.getArea()}`
     }
 }
+
+// const circle1 = new Circle(3);
+// const circle2 = new Circle(2);
+// const circle3 = new Circle(4);
+// const square1 = new Square(3);
+// const square2 = new Square(2);
+// const square3 = new Square(4);
+
+// const shapeArray = [circle1, circle2, circle3, square1, square2, square3]
+
+// Shape Generator Class
+class Generator{
+    static sortShapes(shapeArray){
+        return shapeArray.sort((a, b) => b.getArea() - a.getArea());
+    }
+
+    static generateShapes(){
+        const shapeArray = [];
+        for(let i = 0; i < 100; i++){
+            if(shapeArray.length < 50){
+                shapeArray.push(new Circle((this.randomizeBetween(1, 100)) / 2))
+            } else {
+                shapeArray.push(new Square(this.randomizeBetween(1, 100)))
+            }
+        }
+        return shapeArray;
+    }
+
+    static randomizeBetween(min, max){
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
