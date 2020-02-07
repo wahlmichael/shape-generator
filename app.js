67da4b574405303467e9a3b886bba6d43b0f3571
michaelwahl <michaelwahl9@gmail.com>
2020-02-06 20:16:38 -0600

    create shape constructors

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('working')
+// Circle Constructor
+class Circle{
+    constructor(radius){
+        this.radius = radius;
+    }
+    
+    getArea(){
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    toString(){
+        return `Circle: Radius = ${this.radius}, Area = ${this.getArea()}`
+    }
+}
+
+// Square Constructor
+class Square{
+    constructor(length){
+        this.length = length;
+    }
+
+    getArea(){
+        return Math.pow(this.length, 2);
+    }
+
+    toString(){
+        return `Square: Length = ${this.length}, Area = ${this.getArea()}`
+    }
+}
